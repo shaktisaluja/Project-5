f2b3ec3534a2614f4ac94f5a3375a8ecba8a079a
krish2592 <sachink9225s@gmail.com>
2022-05-25 14:30:35 +0530

    Refactor aws s3

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -3,43 +3,9 @@ const { validateEmail, validatePassword, validateFeild, validateStreet, validate
 const mongoose = require('mongoose')
 const jwt = require("jsonwebtoken");
 const bcrypt = require("bcrypt");
-const aws = require("aws-sdk")
-
-//...............................................AWS CONNECTION..................................................................
-
-aws.config.update({
-  credentials: {
-    accessKeyId: "AKIAY3L35MCRUJ6WPO6J",
-    secretAccessKey: "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"
-  },
-  region: 'ap-south-1'
-})
-
-let uploadFile = async (file) => {
-  return new Promise(function (resolve, reject) {
-
-    // this function will upload file to aws and return the link
-    let s3 = new aws.S3({ apiVersion: '2006-03-01' }); // we will be using the s3 service of aws
-
-    var uploadParams = {
-      ACL: "public-read",
-      Bucket: "classroom-training-bucket",  //HERE
-      Key: "profileImage_v01/" + file.originalname, //HERE
-      Body: file.buffer
-    }
+const {uploadFile} = require('../utilities/uploadFile')
 
 
-    s3.upload(uploadParams, function (err, data) {
-      if (err) {
-        return reject({ "error": err })
-      }
-      console.log(data)
-      console.log("file uploaded succesfully")
-      return resolve(data.Location)
-    })
-
-  })
-}
 //.............................................PHASE (1) Create user................................................
 
 
@@ -164,7 +130,8 @@ const createuser = async (req, res) => {
 
     let files = req.files
     if (files && files.length > 0) {
-      let uploadedFileURL = await uploadFile(files[0])
+      let dirName="profileImage_v01";
+      let uploadedFileURL = await uploadFile(files[0],dirName)
       data.profileImage = uploadedFileURL
     }
     else {
